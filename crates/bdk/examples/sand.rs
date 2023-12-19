fedc8c080fb6e858706bbfd5fff76ec5a63569dc
Jiri Jakes <jiri@jirijakes.com>
2023-12-19 20:41:13 +0800

    Move to signet

--- crates/bdk/examples/sand.rs
+++ crates/bdk/examples/sand.rs
@@ -3,53 +3,44 @@ use std::fmt::Debug;
 use std::str::FromStr;
 
 use bdk::bitcoin::Network::*;
-use bdk::template::{Bip86, P2TR};
 use bdk::wallet::AddressIndex::New;
-use bdk::{KeychainKind::*, Wallet};
+use bdk::Wallet;
 use bdk_chain::{BlockId, ConfirmationTime, PersistBackend};
-use bitcoin::consensus::Encodable;
 use bitcoin::hashes::Hash;
 use bitcoin::{Address, BlockHash, OutPoint, Transaction, TxIn, TxOut, Txid};
 
 pub fn main() {
-    // let key =
-    //     bitcoin::bip32::ExtendedPrivKey::new_master(Network::Signet, &[0xf0, 0x0d, 0xba, 0xbe])
-    //         .unwrap();
+    let mut wallet = mk_wallet();
 
-    // let mut wallet = Wallet::new(
-    //     Bip86(key, External),
-    //     Some(Bip86(key, Internal)),
-    //     Whisper,
-    //     Network::Signet,
-    // )
-    // .unwrap();
+    let addr = Address::from_str("tb1qcweafyulu7samj8qa3cdnnr6axtp6c6wpdt985")
+        .unwrap()
+        .assume_checked();
 
-    let (mut w, tx) = mk_wallet();
+    let mut builder = wallet.build_tx();
+    builder.add_recipient(addr.script_pubkey(), 10000);
+    let mut psbt = builder.finish().unwrap();
 
-    println!("{:?}", w.get_address(New));
+    println!("{:#?}", psbt);
 
-    println!("{:?}", tx.output[0].script_pubkey.is_v0_p2wpkh());
-
-    let mut t = vec![];
-    let _ = tx.consensus_encode(&mut t);
-
-    println!("{}", hex::encode(&t));
+    // let x = wallet.sign(&mut psbt, Default::default());
+    // println!("{:?}", x);
 }
 
 #[rustfmt::skip]
-fn mk_wallet() -> (Wallet<Whisper>, Transaction) {
+fn mk_wallet() -> Wallet<Whisper> {
     let mut wallet = Wallet::new(
-        "wpkh(cVpPVruEDdmutPzisEsYvtST1usBR3ntr8pXSyt6D2YYqXRyPcFW)",
+        // bitcoin::bip32::ExtendedPrivKey::new_master(Signet, &[0]).unwrap()
+        "wpkh(tprv8ZgxMBicQKsPexv3jfDTHG3s59Gh6VwRcgpFzaDG125BnAFyU2MU1h2NbNFqgkgyTds9jy1kxA4tZYcoVh5M1rbLR2aWeUto9iX99XsBmT9/*)",
         None,
         Whisper,
-        Regtest,
+        Signet,
     )
     .unwrap();
 
     let change_address = wallet.get_address(New).address;
-    let sendto_address = Address::from_str("bcrt1q3qtze4ys45tgdvguj66zrk4fu6hq3a3v9pfly5")
+    let sendto_address = Address::from_str("tb1qk2clpn6qht0d5vu4ddqtxw9ddx2jlartgxtync")
         .expect("address")
-        .require_network(Regtest)
+        .require_network(Signet)
         .unwrap();
 
     let tx0 = Transaction {
@@ -85,9 +76,11 @@ fn mk_wallet() -> (Wallet<Whisper>, Transaction) {
     wallet.insert_checkpoint(BlockId {height: 2_000, hash: BlockHash::all_zeros(),}).unwrap();
 
     wallet.insert_tx(tx0, ConfirmationTime::Confirmed {height: 1_000, time: 100,},).unwrap();
-    wallet.insert_tx(tx1.clone(), ConfirmationTime::Confirmed {height: 2_000, time: 200,},).unwrap();
+    wallet.insert_tx(tx1, ConfirmationTime::Confirmed {height: 2_000, time: 200,},).unwrap();
 
-    (wallet, tx1)
+    wallet.commit().unwrap();
+    
+    wallet
 }
 
 pub struct Whisper;
